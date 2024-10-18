50ab48e0c65396f363a746a93f156f8b3412c982
varzzzzzh <163903349+varzzzzzh@users.noreply.github.com>
2024-10-19 00:48:04 +0530

    main page changed

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,11 @@ void main() async {
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
- 
+
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      
+      debugShowCheckedModeBanner: false,
       home: SplashScreen(),
     );
   }
